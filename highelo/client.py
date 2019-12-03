af05eca1ac043f6c7faffdd623a9ea6b17062f3c
Christian Alessandro <christianalessandro2013@gmail.com>
2019-12-03 12:02:37 -0300

    logica de jogo iniciada

--- highelo/client.py
+++ highelo/client.py
@@ -2,10 +2,71 @@
 # import thorpy
 import socket
 import pickle
+import os
 
 # mixer.init()
 # mixer.music.load("C:\\Users\\LAN HOUSE\\Desktop\\agoravai\\fly me to the moon instrumental.mp3")
 # mixer.music.play(-1)
+
+
+pos = ['_', '_', '_', '_', '_', '_', ' ', ' ', ' ']
+stream = []
+
+
+def tab():
+    print("{}|{}|{}\n"
+          "{}|{}|{}\n"
+          "{}|{}|{}\n"
+          .format(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6], pos[7], pos[8]))
+
+
+def mod():
+    print("{}|{}|{}\n"
+          "{}|{}|{}\n"
+          "{}|{}|{}\n"
+          .format(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+
+def update(jogada, jogador):
+    os.system('cls')
+    pos[jogada - 1] = jogador
+    mod()
+    tab()
+
+
+def init(vez):
+    while True:
+        if vez == 1:
+            ordem = 'X'
+            print('você começa')
+            print('escolha uma posição de 1 à 9')
+            jogada = int(input())
+            update(jogada, ordem)
+            sock.send(pickle.dumps(jogada))
+            jogada = pickle.loads(sock.recv(4096))
+            update(jogada, ordem)
+
+        else:
+            ordem = 'O'
+            print('esperando o outro jogador')
+            jogada = pickle.loads(sock.recv(4096))
+            update(jogada, ordem)
+            print('sua vez')
+            mod()
+            tab()
+            print('escolha uma posição de 1 à 9')
+            jogada = int(input())
+            update(jogada, ordem)
+            sock.send(pickle.dumps(jogada))
+
+
+def gui():
+    tab()
+    mod()
+    ordem = pickle.loads(sock.recv(4096))
+    print(ordem)
+    init(ordem)
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
@@ -14,4 +75,4 @@ port = 5555
 sock.connect((host, port))
 data = pickle.loads(sock.recv(4096))
 print(data['sit'])
-input()
+gui()
--- highelo/server.py
+++ highelo/server.py
@@ -1,7 +1,9 @@
 import socket
 import pickle
+from random import randint
 
 gamers = 0
+stream = []
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
@@ -10,14 +12,42 @@ port = 5555
 sock.bind((host, port))
 sock.listen(100)
 
+
+def msg_player(player1, player2):
+    player1.send(pickle.dumps(1))
+    player2.send(pickle.dumps(2))
+    return player1, player2
+
+
+def game_init():
+    ordem = randint(1, 2)
+    if ordem == 1:
+        print('jogador1 começa')
+        p1, p2 = msg_player(sockg1, sockg2)
+    else:
+        print('jogador2 começa')
+        p1, p2 = msg_player(sockg2, sockg1)
+    while True:
+        print(' ')
+        jogada = pickle.loads(p1.recv(4096))
+        stream.append(jogada)
+        p2.send(pickle.dumps(jogada))
+        jogada = pickle.loads(p2.recv(4096))
+        stream.append(jogada)
+        p1.send(pickle.dumps(jogada))
+
+
 while True:
     if gamers == 0:
         codigo = {'sit': 'funciona'}
         sockg1, addr1 = sock.accept()
+        print('jogador 1 conectado')
         gamers += 1
         sockg1.send(pickle.dumps(codigo))
     if gamers == 1:
         codigo = {'sit': 'funciona'}
         sockg2, addr2 = sock.accept()
+        print('jogador 2 conectado')
         gamers += 1
         sockg2.send(pickle.dumps(codigo))
+        game_init()
